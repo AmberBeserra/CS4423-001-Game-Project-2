35e000a8d6cdf82798a59ff73528abcfe48fe781
GraysonWesolowski <wesolowskigrayson@gmail.com>
2022-11-23 10:34:16 -0600

    added gold system
    basic gold system, could not see the shop script at the time so could not work on subtracting gold but will do that very soon.

--- GameDev Project/Assets/Scipts/GameManager.cs
+++ GameDev Project/Assets/Scipts/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,8 +15,12 @@ public class GameManager : MonoBehaviour
 	public static GameManager gameManager{get; private set;}
 	
 	public HitPoint playerHP = new HitPoint(20, 20);
-	
-	void Awake()
+
+    public Text GoldToScore;
+
+	int CurrentScore;
+
+    void Awake()
 	{
 		//if there is another GameManager, destroy one.
 		if(gameManager != null && gameManager != this)
@@ -25,4 +32,39 @@ public class GameManager : MonoBehaviour
 			gameManager = this;
 		}
 	}
+	private void Start()
+	{
+        PlayerPrefs.DeleteKey("gold_score_counter"); //this starts over player gold count to default, remove this line to continue "save data"
+        if (SceneManager.GetActiveScene().name == "FirstScene")
+		{
+            if (PlayerPrefs.HasKey("gold_score_counter"))
+            {
+                CurrentScore = PlayerPrefs.GetInt("gold_score_counter");
+                GoldToScore.text = CurrentScore.ToString();
+            }
+            else
+            {
+                CurrentScore = 100;
+                GoldToScore.text = "100";
+                PlayerPrefs.SetInt("gold_score_counter", 100);
+            }
+        }
+	}
+	void Update()
+	{
+		if (SceneManager.GetActiveScene().name == "FirstScene")
+		{
+			if (Input.GetKeyDown(KeyCode.A))  //for testing purposes
+			{
+				ScoreAfterMonsterDie();
+			}
+		}
+
+    }
+	public void ScoreAfterMonsterDie() 
+	{
+        CurrentScore += 20;
+        GoldToScore.text = CurrentScore.ToString();
+        PlayerPrefs.SetInt("gold_score_counter", CurrentScore);
+    }
 }
