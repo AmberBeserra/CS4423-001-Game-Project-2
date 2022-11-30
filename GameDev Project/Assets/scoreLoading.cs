667638d79e4427dd9b3c5e30d3f50b23880b4ac7
GraysonWesolowski <wesolowskigrayson@gmail.com>
2022-11-30 09:52:31 -0600

    made gold system better
    added a better UI for the gold system, added methods in GameManager for purchasing items in shop

--- GameDev Project/Assets/scoreLoading.cs
+++ GameDev Project/Assets/scoreLoading.cs
@@ -10,14 +10,14 @@ public class scoreLoading : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (GoldToScore != null)
+        if (GoldToScore != null) //checks to make sure GoldToScore is on the scene before providing the counter
         {
-            if (PlayerPrefs.HasKey("gold_score_counter"))
+            if (PlayerPrefs.HasKey("gold_score_counter")) //checks if counter already exists at the correct amount
             {
                 CurrentScore = PlayerPrefs.GetInt("gold_score_counter");
                 GoldToScore.text = CurrentScore.ToString();
             }
-            else
+            else //provides a new counter with a base gold of 100
             {
                 CurrentScore = 100;
                 GoldToScore.text = "100";
