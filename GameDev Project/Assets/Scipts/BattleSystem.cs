50bd131ddc24fda8f7a89e59e4db8147ba31ab39
AmberBeserra <AmberBeserra@gmail.com>
2022-12-01 15:38:58 -0600

    adding animations/touchup

--- GameDev Project/Assets/Scipts/BattleSystem.cs
+++ GameDev Project/Assets/Scipts/BattleSystem.cs
@@ -42,8 +42,8 @@ public class BattleSystem : MonoBehaviour
     }
     IEnumerator Setup()
     {
-        potion1.text = "Potion1: " + FindObjectOfType<GameManager>().GetPotions();
-        potion2.text = "Potion2: " + FindObjectOfType<GameManager>().GetPotions2();
+        //potion1.text = "Potion1: " + FindObjectOfType<GameManager>().GetPotions();
+        //potion2.text = "Potion2: " + FindObjectOfType<GameManager>().GetPotions2();
 
         playerUnit = GameObject.Find("PCfacingright").GetComponent<Unit>();
         enemyUnit = GameObject.Find("slime").GetComponent<Unit>();
