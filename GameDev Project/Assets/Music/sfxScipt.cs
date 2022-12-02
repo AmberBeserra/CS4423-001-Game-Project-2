32c717e417567ee9868c237f7138d66384b6b4ec
NathanHartley <61166000+NathanHartley@users.noreply.github.com>
2022-12-02 00:27:18 -0800

    added button sounds for final battle
    Added sounds for final battle and fixed sounds for first battle (missing menu button sounds)

--- GameDev Project/Assets/Music/sfxScipt.cs
+++ GameDev Project/Assets/Music/sfxScipt.cs
@@ -8,6 +8,7 @@ public class sfxScipt : MonoBehaviour
     public AudioSource goldSound;
     public AudioSource swordSound;
     public AudioSource blockSound;
+    public AudioSource swordMissSound;
 
     public void playMenuClick ()
     {
@@ -19,13 +20,18 @@ public class sfxScipt : MonoBehaviour
        goldSound.Play();
     }
 
-    public void playBlockSound()
+    public void playSwordSound()
     {
         swordSound.Play();
     }
 
-    public void playSwordSound()
+    public void playBlockSound()
     {
         blockSound.Play();
+    }
+
+    public void playSwordMissSound()
+    {
+        swordMissSound.Play();
     }
 }
